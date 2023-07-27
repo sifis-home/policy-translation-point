a027b24203b5ddbb3e37b9d526bcfa498f6d5f10
monge <alberto.monge@polito.it>
2023-07-27 14:48:28 +0200

    create db

--- scripts/build-ptp.sh
+++ scripts/build-ptp.sh
@@ -15,7 +15,7 @@ then
   sudo systemctl start mysql.service
   echo "root" > db.pwd # Root username
   echo "rootroot" >> db.pwd # Root pw
-  sudo mysql -u root -prootroot < schema.sql
+  sudo mysql -h 127.0.0.1 --port 3306 -u root -prootroot < schema.sql
 fi
 
 # Run mvn on ace-entities
